7cf034f23e8b3b4c3e3e88bb0bf438d40793bb14
David Zemon <david@zemon.name>
2023-05-30 09:34:53 -0500

    Push a bunch more settings for Token standards

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN pip3 install -r requirements.txt
 COPY . .
 
 ENTRYPOINT ["python3", "main.py"]
-EXPOSE 9684
+EXPOSE 7593
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import os
 
@@ -44,11 +43,23 @@ async def post(installation_id: int, url: str, data: any) -> any:
         )
 
 
+async def patch(installation_id: int, url: str, data: any) -> any:
+    async with aiohttp.ClientSession() as session:
+        gh = GitHubAPI(session, "token-gh-bot")
+        access_token = await produce_access_token(gh, installation_id)
+        return await gh.patch(
+            url,
+            oauth_token=access_token,
+            data=data
+        )
+
+
 @router.register(event_type="repository", action="created")
 async def handle_new_repository_event(event: sansio.Event):
     repo_full_name = event.data['repository']['full_name']
+    installation_id = event.data['installation']['id']
 
-    response = await post(event.data['installation']['id'], f"/repos/{repo_full_name}/rulesets", {
+    await post(installation_id, f"/repos/{repo_full_name}/rulesets", {
         "name": "Protected branch",
         "target": "branch",
         "source_type": "Repository",
@@ -86,7 +97,29 @@ async def handle_new_repository_event(event: sansio.Event):
             }
         ],
     })
-    LOGGER.info(response)
+
+    for prefix in ['FW', 'SW', 'APP', 'SE']:
+        prefix = prefix.upper()  # Just in case someone screws up the list
+        await post(installation_id, f"/repos/{repo_full_name}/autolinks", {
+            "key_prefix": f"{prefix}-",
+            "url_template": f"https://tokenring.atlassian.net/browse/{prefix}-<num>",
+            "is_alphanumeric": False
+        })
+
+    await patch(installation_id, f"/repos/{repo_full_name}", {
+        "has_issues": False,
+        "has_projects": True,
+        "has_downloads": True,
+        "has_wiki": False,
+        "has_pages": False,
+        "has_discussions": False,
+        "allow_rebase_merge": False,
+        "allow_squash_merge": False,
+        "allow_update_branch": True,
+        "allow_auto_merge": True,
+        "delete_branch_on_merge": True,
+        "web_commit_signoff_required": False,
+    })
 
 
 async def gh_event_handler(request: aiohttp.web.Request):
@@ -100,6 +133,9 @@ async def gh_event_handler(request: aiohttp.web.Request):
 
 
 if __name__ == "__main__":
+    # Check that we can find our key. Fail fast if we can't.
+    jwt_generator.generate()
+
     app = web.Application()
     app.router.add_post("/gh-hook", gh_event_handler)
     web.run_app(app, port=int(os.getenv('PORT', '7593')))
